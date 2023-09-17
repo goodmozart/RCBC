cde4059d726cf31437d4699cdd1ba337d6965046
Aljun <Aljun@LAPTOP-R9DB2QJD>
2023-09-17 11:19:51 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RCBC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
